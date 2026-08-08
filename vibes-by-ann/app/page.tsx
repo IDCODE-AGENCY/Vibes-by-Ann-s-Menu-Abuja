c992232d029b2ce5934be53352b0dcf47edd426d
Dahiru Usman Hassan <usmandahirub@gmail.com>
2026-08-08 04:40:55 +0000

    vibes by ann's menu

---
+++ vibes-by-ann/app/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import dynamic from 'next/dynamic';
+
+const PdfViewer = dynamic(() => import('@/components/PdfViewer'), {
+  ssr: false,
+});
+
+export default function Home() {
+  return (
+    <main className="w-full min-h-screen">
+      <PdfViewer url="/vba.pdf" />
+    </main>
+  );
+}
